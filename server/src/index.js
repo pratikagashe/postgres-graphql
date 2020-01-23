1540de23e6bcf960208b7711ae439398a7cf8f5f
pratikagashe <pratikagashe007@gmail.com>
2020-01-23 16:21:37 +0530

    postgraphile middleware added

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,29 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const { postgraphile } = require('postgraphile')
 
-const { PORT } = process.env
+const { PORT, DATABASE, USER, PASSWORD, HOST, PG_PORT } = process.env
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(
+    postgraphile(
+        {
+            database: DATABASE,
+            user: 'postgres',
+            password: PASSWORD,
+            host: HOST,
+            port: PG_PORT,
+        },
+        'public',
+        {
+            watchPg: true,
+            graphiql: true,
+            enhanceGraphiql: true,
+        }
+    )
+)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
