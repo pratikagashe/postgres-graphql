14fea0ac82502c881073c440347877979c1a6934
pratikagashe <pratikagashe007@gmail.com>
2020-01-28 19:58:19 +0530

    Server graphql url updated, cors updated

--- client/src/ApolloClient.tsx
+++ client/src/ApolloClient.tsx
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 const cache = new InMemoryCache()
 const link = new HttpLink({
-  uri: 'http://localhost:8080/graphql/',
+  uri: 'http://localhost:8080/graphql',
 })
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,15 @@
 require('dotenv').config()
+const cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const postgraphile = require('./postgraphile')
-const cors = require('cors')
 
 const { PORT } = process.env
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(postgraphile)
-app.use(cors)
+app.use(cors())
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
